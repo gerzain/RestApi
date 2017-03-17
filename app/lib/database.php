54af1fe8ee592bd2c474e52b112136613de436d0
Irving <irving.java@gmail.com>
2017-03-17 14:04:43 -0600

    Reducir sql injection de nivel 2

--- app/lib/database.php
+++ app/lib/database.php
@@ -11,7 +11,7 @@ class Database
         
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         return $pdo;
     }
 }
