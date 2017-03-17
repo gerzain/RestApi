62622f44d8423a06e25f5fa6700e1dcf4e8d19a7
Irving <irving.java@gmail.com>
2017-03-17 14:05:29 -0600

    Agregar View a base de datos para obtener clientes

--- app/model/user_model.php
+++ app/model/user_model.php
@@ -9,26 +9,26 @@ class UserModel
     private $db;
     private $table = 'empleado';
     private $response;
-    
+
     public function __CONSTRUCT()
     {
         $this->db = Database::StartUp();
         $this->response = new Response();
     }
-    
+
     public function GetAll()
     {
 		try
 		{
 			$result = array();
 
-			$stm = $this->db->prepare("SELECT * FROM $this->table");
-			$stm->execute();
-            
-			$this->response->setResponse(true);
-            $this->response->result = $stm->fetchAll();
-            
-            return $this->response;
+            $stm = $this->db->prepare("SELECT * FROM getempleados");
+            $stm->execute();
+
+			    $this->response->setResponse(true);
+          //  $this->response->result = $stm->fetchAll();
+            return $stm->fetchAll();
+          //  return $this->response;
 		}
 		catch(Exception $e)
 		{
@@ -36,7 +36,7 @@ class UserModel
             return $this->response;
 		}
     }
-    
+
     public function Get($id)
     {
 		try
@@ -45,27 +45,28 @@ class UserModel
 
 			$stm = $this->db->prepare("SELECT * FROM $this->table WHERE id = ?");
 			$stm->execute(array($id));
+        return $stm->fetchAll();
 
-			$this->response->setResponse(true);
-            $this->response->result = $stm->fetch();
-            
-            return $this->response;
+			//$this->response->setResponse(true);
+          //  $this->response->result = $stm->fetch();
+
+          //  return $this->response;
 		}
 		catch(Exception $e)
 		{
 			$this->response->setResponse(false, $e->getMessage());
             return $this->response;
-		}  
+		}
     }
-    
+
     public function InsertOrUpdate($data)
     {
-		try 
+		try
 		{
             if(isset($data['id']))
             {
-                $sql = "UPDATE $this->table SET 
-                            Nombre          = ?, 
+                $sql = "UPDATE $this->table SET
+                            Nombre          = ?,
                             Apellido        = ?,
                             Correo          = ?,
                             Sexo            = ?,
@@ -73,11 +74,11 @@ class UserModel
                             Profesion_id    = ?,
                             FechaNacimiento = ?
                         WHERE id = ?";
-                
+
                 $this->db->prepare($sql)
                      ->execute(
                         array(
-                            $data['Nombre'], 
+                            $data['Nombre'],
                             $data['Apellido'],
                             $data['Correo'],
                             $data['Sexo'],
@@ -93,11 +94,11 @@ class UserModel
                 $sql = "INSERT INTO $this->table
                             (Nombre, Apellido, Correo, Sexo, Sueldo, Profesion_id, FechaNacimiento, FechaRegistro)
                             VALUES (?,?,?,?,?,?,?,?)";
-                
+
                 $this->db->prepare($sql)
                      ->execute(
                         array(
-                            $data['Nombre'], 
+                            $data['Nombre'],
                             $data['Apellido'],
                             $data['Correo'],
                             $data['Sexo'],
@@ -106,29 +107,29 @@ class UserModel
                             $data['FechaNacimiento'],
                             date('Y-m-d')
                         )
-                    ); 
+                    );
             }
-            
+
 			$this->response->setResponse(true);
             return $this->response;
-		}catch (Exception $e) 
+		}catch (Exception $e)
 		{
             $this->response->setResponse(false, $e->getMessage());
 		}
     }
-    
+
     public function Delete($id)
     {
-		try 
+		try
 		{
 			$stm = $this->db
-			            ->prepare("DELETE FROM $this->table WHERE id = ?");			          
+			            ->prepare("DELETE FROM $this->table WHERE id = ?");
 
 			$stm->execute(array($id));
-            
+
 			$this->response->setResponse(true);
             return $this->response;
-		} catch (Exception $e) 
+		} catch (Exception $e)
 		{
 			$this->response->setResponse(false, $e->getMessage());
 		}
